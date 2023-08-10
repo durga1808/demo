c2f9552e14166f6e5793524830501db8c6d1f2bb
durga1808 <durgalskshmi3713@gmail.com>
2023-08-10 22:38:17 +0530

    added 2nd branch

--- src/main/java/com/examp/App.java
+++ src/main/java/com/examp/App.java
@@ -2,6 +2,8 @@ package com.examp;
 
 public class App 
 {
+    int a;
+    String abc; //initialize the variable to second branch
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );//print the line
